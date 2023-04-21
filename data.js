202296c002257a549a5433719a4249c0f0290ca8
wanjikumwas <99952287+wanjikumwas@users.noreply.github.com>
2023-04-21 09:48:20 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,234 @@
+
+let hospitals = [ 
+    {
+       "name": "Plainview Hospital",
+       "phoneNumber":"0711717863",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": -1.15543,
+           "longitude": 36.95951, 
+       },
+   
+       "address":" Thika Road, Ruiru, 00232",
+       "rating" : "3.2",
+       "reviews": "108 ",
+        
+   },
+   
+   {
+       "name": "Arise hospital",
+       "phoneNumber":"0798649970",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": 1.1684,
+           "longitude": 36.9746,
+       },
+       "address":" Along Eastern Bypass at Kamakis (Destiny area), Ruiru, Kiambu",
+       "rating" : "4.1",
+       "reviews": "24 "
+   },
+   
+   {
+       "name": "Ruiru subcounty Hospital",
+       "phoneNumber":"0724616353",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": 1.1446,
+           "longitude": 36.9557,
+       },
+       "address":"VX44+689, Unnamed Road Biashara Ruiru Kiambu KE, Ruiru",
+       "rating" : "4.2",
+       "reviews": " 26 "
+   },
+   
+   
+   {
+       "name": "Ruiru outpatient and specialist centre",
+       "phoneNumber":"0721494021",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": -1.1482,
+           "longitude": 36.9584,
+       },
+       "address":" Doctors Plaza, Mathigu Road, Ruiru, Ruiru",
+       "rating" : "3.5",
+       "reviews": "4"
+   },
+   
+   {
+       "name": "RFH healthcare",
+       "phoneNumber":"0111033800",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": -1.1706,
+           "longitude": 36.9722,
+       },
+       "address":"Near Total Eastern Bypass, Eastern Bypass, After Kamakis Area, Ruiru",
+       "rating" : "No ratings",
+       "reviews": "No reviews "
+   },
+   
+   
+   {
+       "name": "Nazareth Ruiru hospital",
+       "phoneNumber":"0202017401",
+       "openTime" : "Open 8 am,close 8 pm",
+       "coordinates": {
+           "latitude": 1.1466,
+           "longitude": 36.9471,
+       },
+       "address":"Biashara Hospital Rd",
+       "rating" : "3.7",
+       "reviews": "47"
+   },
+   
+   
+   {
+       "name": "Highcure medical centre",
+       "phoneNumber":"0112 590423",
+       "openTime" : "Open 7:30 am,closes 8 pm",
+       "coordinates": {
+           "latitude": 1.1597,
+           "longitude": 36.9395,
+       },
+       "address":"Membley Eastern Bypass Opposite Redeemed Church ",
+       "rating" : "5.0",
+       "reviews": "3"
+   },
+   
+   
+   {
+       "name": "Everest medical centre",
+       "phoneNumber":"Not Available",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": 1.1474,
+           "longitude": 36.9610,
+       },
+       "address":"2nd Floor, CK Building, Opposite Bushgate Towers, Ruiru Kamiti Road, Ruiru",
+       "rating" : "No ratings",
+       "reviews": "No reviews "
+   },
+   
+   
+   {
+       "name": "Equity Afia Ruiru hospital",
+       "phoneNumber":" 0765 000006",
+       "openTime" : "Open 8 am,closes 8 pm",
+       "coordinates": {
+           "latitude": -1.1482,
+           "longitude": 36.9584,
+       },
+       "address":"Lilly House, Mathigu Road, Ruiru",
+       "rating" : "4.4",
+       "reviews": "21 "
+   },
+   
+   
+   {
+       "name": "Genus Ruiru Family Hospital",
+       "phoneNumber":" 0724 670888",
+       "openTime" : "Open 24 Hours",
+       "coordinates": {
+           "latitude": -1.1706,
+           "longitude": 36.9722,
+       },
+       "address":" Eastern Bypass, 70mtrs from Ruiru Bypass Roundabout, Near Delta Fuel Station, Ruiru",
+       "rating" : "1.0",
+       "reviews": "2 "
+   
+   },
+   
+   {
+       "name": "Hopkins Medical centre,Membley Ruiru",
+       "phoneNumber":"0705878746",
+       "openTime" : " 24 hours",
+       "coordinates": {
+           "latitude": 1.1630,
+           "longitude": 36.9286,
+       },
+       "address":"RWQV+M5V, Ruiru",
+       "rating" : "4.0",
+       "reviews": "1 "
+   },
+   
+   {
+       "name": "Kimbo Health Centre",
+       "phoneNumber":"Not available",
+       "openTime" : "Open 24 hours",
+       "coordinates": {
+           "latitude": -1.1325,
+           "longitude": 36.9744,
+       },
+       "address":"VXC9+2XG, Ruiru",
+       "rating" : "2.0",
+       "reviews": "2 "
+   },
+   
+   {
+       "name": "Gewa Hospital",
+       "phoneNumber":"Not Available",
+       "openTime" : "Opens 24 Hours",
+       "coordinates": {
+           "latitude": 1.1467,
+           "longitude": 36.9597,
+       },
+       "address":"VX35+8VG, Near Father's House Family Church, Ruiru Kamiti Road, Ruiru ",
+       "rating" : "No ratings",
+       "reviews": "No reviews"
+   },
+   
+   {
+       "name": "Ruiru Health Clinic",
+       "phoneNumber":" 067 5854115",
+       "openTime" : "Opens 24 hours",
+       "coordinates": {
+           "latitude": 1.1438,
+           "longitude": 36.9563,
+       },
+       "address":"Biashara St, Kiambu ",
+       "rating" :"No ratings",
+       "reviews": "No reviews "
+   },
+   
+   {
+       "name": "Saint Joseph Medical Centre",
+       "phoneNumber":"Not Available",
+       "openTime" : "Open 24 hours",
+       "coordinates": {
+           "latitude": 1.1475,
+           "longitude": 36.9606,
+       },
+       "address":"VX26+X6W, Ruiru",
+       "rating" : "5.0",
+       "reviews": "1 "
+   },
+   
+   {
+       "name": "Kenyatta Universiy Teaching,Referral and Research Hospital",
+       "phoneNumber":"0800721038",
+       "openTime" : "Opens 24 hours",
+       "coordinates": {
+           "latitude": -1.1776,
+           "longitude": 36.9154,
+       },
+       "address":"Along Northern Bypass, Kahawa West, Nairobi",
+       "rating" : "4.0",
+       "reviews": "157 "
+   },
+   
+   {
+       "name": "Afyacross Medical hospital",
+       "phoneNumber":"0115507667",
+       "openTime" : "Opens 9 am,closes 9 pm",
+       "coordinates": {
+           "latitude": -1.1327,
+           "longitude": 36.9757,
+       },
+       "address":" Matangi Road, Ruiru",
+       "rating" : "5.0",
+       "reviews": "3"
+   },
+   ]
+   
+   
